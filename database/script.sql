f418c4abb748478b666281f67ef4991d18833620
Hebert <hebertLandim007@outlook.com>
2024-01-11 15:08:30 -0300

    feat: add data access object (DAO) to user entity

--- database/script.sql
+++ database/script.sql
@@ -4,13 +4,12 @@ use banco_de_usuarios;
 
 create table usuario(
 	id integer auto_increment primary key,
-    nome_completo varchar(200) not null,
-    username varchar(100) not null unique,
+    nome varchar(100) not null unique,
     email varchar(50) not null unique,
     senha text not null,
     telefone varchar(15) not null
 );
 
-insert into usuario values (null, 'João Silva', 'joao_dev', 'joao.silva@email.com', 'senhajoao', '11999887676');
+insert into usuario values (null, 'joao_dev', 'joao.silva@email.com', 'senhajoao', '11999887676');
 
 select * from usuario;

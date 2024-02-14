64752f339a9f7b3b97812b8c96b074673d32eb48
Hebert <hebertLandim007@outlook.com>
2024-02-14 17:35:47 -0300

    feat: add Lombok to simplify getters and setters

--- backend/projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
+++ backend/projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
@@ -1,8 +1,9 @@
 package br.com.criandoapi.projeto.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
-
+@Data
 @Entity
 @Table(name = "usuario")
 public class Usuario {
@@ -19,34 +20,4 @@ public class Usuario {
     @Column(name = "telefone", length = 15, nullable = true)
     private String telefone;
 
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getSenha() {
-        return senha;
-    }
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-    public String getTelefone() {
-        return telefone;
-    }
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
 }
